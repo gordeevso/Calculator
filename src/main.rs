d9a51f8d007b11809e3bdfe191e5b78234111428
sgordeev <gordeev.so@yandex.ru>
2020-03-22 16:53:46 +0300

    Refactor & remove wall of code

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,108 @@
 
 use std::io;
-use std::collections::hash_map::{HashMap};
+use std::collections::hash_map::HashMap;
 
-const OPERATOR_PLUS: &char = &'+';
-const OPERATOR_MINUS: &char = &'-';
-const OPERATOR_MULTIPLY: &char = &'*';
-const OPERATOR_DIVIDE: &char = &'/';
+const OPERATOR_PLUS: u8 = b'+';
+const OPERATOR_MINUS: u8 = b'-';
+const OPERATOR_MULTIPLY: u8 = b'*';
+const OPERATOR_DIVIDE: u8 = b'/';
+const LEFT_BRACE: u8 = b'(';
+const RIGHT_BRACE: u8 = b')';
 
-fn tokenize(expression: String) -> Vec<String> {
-    let mut tokens: Vec<String> = Vec::new();
-    let mut number = String::with_capacity(10);
-    for c in expression.chars() {
-        if c.is_whitespace() {
-            continue;
-        } else if c.is_numeric() {
-            number.push(c);
-        } else if c == '+' || c == '-'|| c == '/' || c == '*' || c == '(' || c == ')' {
-            if number.len() > 0 {
-                tokens.push(number.clone());
-            }
-            number.clear();
-            tokens.push(c.to_string());
-        } else {
-            panic!("Unexpected symbol '{}'", c);
-        }
+type AsciiBytes = Vec<u8>;
+
+fn is_operator(byte: &u8) -> bool {
+    match *byte {
+        OPERATOR_PLUS |
+        OPERATOR_MINUS |
+        OPERATOR_MULTIPLY |
+        OPERATOR_DIVIDE => true,
+        _ => false
     }
-    if number.len() > 0 {
-        tokens.push(number);
-    }
-    tokens
 }
 
-fn main() {
-    let mut expression = String::new();
-    io::stdin().read_line(&mut expression).unwrap();
-    println!("Input expression: {}", expression);
+fn is_bracket(byte: &u8) -> bool {
+    match *byte {
+        LEFT_BRACE | RIGHT_BRACE => true,
+        _ => false
+    }
+}
 
-    let tokens = tokenize(expression);
-    for token in &tokens {
-        println!("{}", token);
+fn tokens_from_ascii(expression_ascii: &[u8]) -> Vec<AsciiBytes> {
+    let mut tokens_ascii: Vec<AsciiBytes> = Vec::new();
+    let mut number: AsciiBytes = Vec::new();
+
+    for &byte in expression_ascii {
+        if byte.is_ascii_whitespace() {
+            continue;
+        } else if byte.is_ascii_digit() {
+            number.push(byte);
+        } else if is_operator(&byte) || is_bracket(&byte) {
+            if number.len() > 0 {
+                tokens_ascii.push(number.clone());
+                number.clear();
+            }
+            number.push(byte);
+            tokens_ascii.push(number.clone());
+            number.clear();
+        } else {
+            panic!("Unexpected symbol {}", byte as char);
+        }
     }
 
+    if number.len() > 0 {
+        tokens_ascii.push(number.clone());
+        number.clear();
+    }
+
+    tokens_ascii
+}
+
+fn reversed_polish_from_tokens_ascii(tokens_ascii: &Vec<AsciiBytes>) -> Vec<AsciiBytes> {
     let mut operator_priority_map = HashMap::new();
     operator_priority_map.insert(OPERATOR_PLUS, 0);
     operator_priority_map.insert(OPERATOR_MINUS, 0);
     operator_priority_map.insert(OPERATOR_MULTIPLY, 1);
     operator_priority_map.insert(OPERATOR_DIVIDE, 1);
 
-    let mut reversed_polish: Vec<String> = Vec::new();
-    let mut stack: Vec<char> = Vec::new();
-    for token in &tokens {
-        for c in token.chars() {
-            if c.is_numeric() {
+    let mut reversed_polish: Vec<AsciiBytes> = Vec::new();
+    let mut stack: Vec<AsciiBytes> = Vec::new();
+
+    for token in tokens_ascii.iter() {
+        for &byte in token.iter() {
+            if byte.is_ascii_digit() {
                 reversed_polish.push(token.clone());
                 break;
-            } else if c == '(' {
-                stack.push(c);
+            } else if byte == LEFT_BRACE {
+                stack.push(token.clone());
                 break;
-            } else if c == ')' {
+            } else if byte == RIGHT_BRACE {
                 loop {
-                    let result = stack.pop();
-                    match result {
+                    match stack.pop() {
                         None => panic!("Can't find matching brace"),
                         Some(value) => {
-                            if value == '(' {
+                            if *value.get(0).unwrap() == LEFT_BRACE {
                                 break;
                             } else {
-                                reversed_polish.push(value.to_string());
+                                reversed_polish.push(value);
                             }
                         }
                     }
                 }
                 break;
             } else {
-                let result = operator_priority_map.get(&c);
-                match result {
-                    None => panic!("Wrong expression, unexpected binary operation '{}'", c),
+                match operator_priority_map.get(&byte) {
+                    None => panic!("Wrong expression, unexpected binary operation '{}'", byte),
                     Some(priority) => {
                         loop {
                             match stack.last() {
                                 None => break,
-                                Some(value) => {
-                                    let stack_top_result = operator_priority_map.get(value);
-                                    match stack_top_result {
+                                Some(operation_top_stack) => {
+                                    match operator_priority_map.get(operation_top_stack.get(0).unwrap()) {
                                         None => break,
-                                        //panic!("Something strange happened, current value {}", c),
-                                        Some(stack_top_priority) => {
-                                            if stack_top_priority >= priority {
-                                                reversed_polish.push(value.to_string());
+                                        Some(priority_top_stack) => {
+                                            if priority_top_stack >= priority {
+                                                reversed_polish.push(operation_top_stack.clone());
                                                 stack.pop();
                                             } else {
                                                 break;
@@ -99,7 +114,7 @@ fn main() {
                         }
                     }
                 }
-                stack.push(c);
+                stack.push(token.clone());
                 break;
             }
         }
@@ -109,38 +124,65 @@ fn main() {
         match stack.pop() {
             None => break,
             Some(value) => {
-                reversed_polish.push(value.to_string());
+                reversed_polish.push(value);
             }
         }
     }
 
-    println!("Reversed polish");
-    for item in &reversed_polish {
-        print!("{} ", item);
-    }
-    println!("");
+    reversed_polish
+}
 
-    let mut calculation_stack: Vec<f32> = Vec::new();
-    for item in &reversed_polish {
-        let first_symbol = item.chars().next().unwrap();
-        if first_symbol.is_numeric() {
-            let number: f32 = item.parse().unwrap();
-            calculation_stack.push(number);
+fn eval_reversed_polish(reversed_polish_tokens: &Vec<AsciiBytes>) -> f32 {
+    let mut eval_stack: Vec<f32> = Vec::new();
+
+    for token in reversed_polish_tokens.iter() {
+        let &first_byte = token.get(0).unwrap();
+        if first_byte.is_ascii_digit() {
+            let token_utf8 = String::from_utf8(token.clone()).unwrap();
+            let number: f32 = token_utf8.parse().unwrap();
+            eval_stack.push(number);
         } else {
-            let right = calculation_stack.pop().unwrap();
-            let left = calculation_stack.pop().unwrap();
+            let right = eval_stack.pop().unwrap();
+            let left = eval_stack.pop().unwrap();
             let result;
-            match &first_symbol {
+            match first_byte {
                 OPERATOR_PLUS => result = left + right,
                 OPERATOR_MINUS => result = left - right,
                 OPERATOR_MULTIPLY => result = left * right,
                 OPERATOR_DIVIDE => result = left / right,
                 _ => result = 0.0,
             }
-            calculation_stack.push(result);
+            eval_stack.push(result);
         }
     }
 
-    let result = calculation_stack.pop().unwrap();
-    println!("Result = {}", result);
+    eval_stack.pop().unwrap()
+}
+
+fn main() {
+    let mut expression = String::new();
+    println!("Please, enter an expression to calculate");
+    io::stdin().read_line(&mut expression).unwrap();
+    print!("Input expression: {}", expression);
+
+    let tokens_ascii = tokens_from_ascii(expression.as_bytes());
+
+    println!("Tokenization:");
+    for (i, token) in tokens_ascii.iter().enumerate() {
+        let token = String::from_utf8(token.clone()).unwrap();
+        println!("Current token {} - '{}'", i, token);
+    }
+    println!("");
+
+    let reversed_polish_tokens = reversed_polish_from_tokens_ascii(&tokens_ascii);
+
+    println!("Reversed polish:");
+    for (i, token) in reversed_polish_tokens.iter().enumerate() {
+        let token = String::from_utf8(token.clone()).unwrap();
+        println!("Current token {} - '{}'", i, token);
+    }
+    println!("");
+
+    let eval_result = eval_reversed_polish(&reversed_polish_tokens);
+    println!("Result = {}", eval_result);
 }
